f101a33554b8cd65127009a7df730ff6312602f1
dev-saiful <saifulcseian@gmail.com>
2023-08-20 00:05:28 +0600

    auth functions created

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -1,4 +1,15 @@
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+ export const comparePasswords = (password,hash)=>{
+    return bcrypt.compare(password,hash);
+}
+
+export const hashPasswords = (password)=>{
+    return bcrypt.hash(password,5);
+}
+
+
 
 export const createJwt = (user)=>{
     const token = jwt.sign(
@@ -13,12 +24,34 @@ export const createJwt = (user)=>{
 }
 
 // protect routes
-export const protect = (req,res)=>{
+export const protect = (req,res,next)=>{
     const bearer = req.headers.authorization;
     if(!bearer)
     {
         res.status(401);
         res.json({message:"you are not authorized!!!"});
+        return;
+    }
+
+    const [,token] = bearer.split(" ");
+    if(!token)
+    {
+        res.status(401);
+        res.json({message:"Invalid token!!"});
+        return;
+    }
+
+    try 
+    {
+        const user = jwt.verify(token,process.env.JWT_SECRECT);
+        req.user = user;
+        next();
+    }
+    catch (e)
+    {
+        console.error(e);
+        res.status(401);
+        res.json({message:"Invalid token!!"});
         return;
     }
 }
