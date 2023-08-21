654433f6b0842c6067de0c1ca1125561f3c6872c
dev-saiful <saifulcseian@gmail.com>
2023-08-21 20:14:13 +0600

    updates handler and routes completed

--- src/handlers/update.ts
+++ src/handlers/update.ts
@@ -9,7 +9,7 @@ export const getOneUpdates = async(req,res)=>{
 
     res.json({data:update});
 }
-export const getUpdate = async(req,res)=>{
+export const getUpdates = async(req,res)=>{
     const products = await prisma.product.findMany({
         where:{
             belongsToId:req.user.id
@@ -24,6 +24,81 @@ export const getUpdate = async(req,res)=>{
     },[]);
     res.json({date:updates});
 }
-export const createUpdate = async(req,res)=>{}
-export const updateUpdate = async(req,res)=>{}
-export const deleteUpdate = async(req,res)=>{}
+export const createUpdate = async(req,res)=>{
+    const product = await prisma.product.findUnique({
+        where:{
+            id:req.body.id
+        }
+    });
+
+    if(!product)
+    {
+        // does not belong to user
+        return res.json({message:"nope!"});
+    }
+
+    const update = await prisma.update.create({
+        data:req.body
+    });
+
+    res.json({data:update});
+}
+export const updateUpdate = async(req,res)=>{
+    const products = await prisma.product.findMany({
+        where:{
+            belongsToId:req.user.id,
+        },
+        include:{
+            updates:true
+        }
+    });
+
+    const updates = products.reduce((allUpdates,product)=>{
+        return [...allUpdates,...product.updates];
+    },[]);
+
+    const match = updates.find((update)=>update.id === req.params.id);
+    if(!match)
+    {
+        // handle
+        return res.json({message:'nope'});
+    }
+
+    const updateUpdate = await prisma.update.update({
+        where:{
+           id:req.params.id
+        },
+        data:req.body
+    });
+
+    res.json({data:updateUpdate});
+}
+export const deleteUpdate = async(req,res)=>{
+    const products = await prisma.product.findMany({
+        where:{
+            belongsToId:req.user.id,
+        },
+        include:{
+            updates:true
+        }
+    });
+
+    const updates = products.reduce((allUpdates,product)=>{
+        return [...allUpdates,...product.updates];
+    },[]);
+
+    const match = updates.find((update)=>update.id === req.params.id);
+    if(!match)
+    {
+        // handle
+        return res.json({message:'nope'});
+    }
+
+    const deleted = await prisma.update.delete({
+        where:{
+            id:req.params.id
+        }
+    });
+
+    res.json({data:deleted});
+}
--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import {Router} from 'express';
 import { body, oneOf, validationResult } from 'express-validator';
 import { hanldeErrors } from './modules/middleware';
 import { getOneProduct, getProducts,createProduct, deleteProduct, updateProduct } from './handlers/product';
+import { createUpdate, deleteUpdate, getOneUpdates, getUpdates, updateUpdate } from './handlers/update';
 
 const router = Router();
 
@@ -17,19 +18,22 @@ router.delete('/product/:id',deleteProduct);
 /**
  * Upadte
  */
-router.get('/update',()=>{});
-router.get('update/:id',()=>{});
+router.get('/update',getUpdates);
+router.get('update/:id',getOneUpdates);
 router.post('/update',
 body('title').exists().isString(),
 body('body').exists().isString(),
-()=>{});
+body('productId').exists().isString(),
+createUpdate
+);
 router.put('update/:id',
 body('title').optional(),
 body('body').optional(),
-body('status').isIn(['IN_PROGRESS','SHIPPED','DEPRICATED']),
+body('status').isIn(['IN_PROGRESS','SHIPPED','DEPRICATED']).optional(),
 body('version').optional,
-()=>{});
-router.delete('update/:id',()=>{});
+updateUpdate
+);
+router.delete('update/:id',deleteUpdate);
 
 /**
  * Upadte Point
